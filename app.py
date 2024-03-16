76f245ea746962217f5c9e8ee7d4662f5a0b945f
ilovetensor <rohanshar11@gmail.com>
2024-03-17 02:01:50 +0530

    hypothesis testing and abtesting steramlit app created

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+from st_pages import Page, show_pages, add_page_title
+
+
+st.set_page_config(layout="wide")
+
+
+
+
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+show_pages(
+    [
+        Page("app.py", "Hypothesis Testing", ""),
+        Page("pages/sample_size.py", "Sample Size Calculator", ""),
+    ]
+)
+
+def perform_ab_test(control_visitors, control_conversions, treatment_visitors, treatment_conversions, confidence_level):
+    
+    # Calculate conversion rates
+    control_rate = control_conversions / control_visitors
+    treatment_rate = treatment_conversions / treatment_visitors
+    
+    # Calculate pooled standard error 
+    pooled_se = np.sqrt(control_rate * (1 - control_rate) / control_visitors + treatment_rate * (1 - treatment_rate) / treatment_visitors)
+    
+    # Calculate Z-score
+    z_score = (treatment_rate - control_rate) / pooled_se
+    
+    # Calculate p-value using the standard normal distribution 
+    p_value = 2 * (1 - norm.cdf(abs(z_score)))
+    
+    # Compare p-value with significance level 
+    significance_level = 1 - confidence_level / 100
+    
+    if p_value < significance_level:
+        if z_score > 0:
+            return "Experiment Group is Better"
+        else:
+            return "Control Group is Better"
+    else:
+        return "Indeterminate"
+    
+
+
+st.title('Fitfly - Data Science Intern Assignment')
+st.subheader('Hypothesis Testing ✅')
+st.markdown('Enter the number of visitors and conversions for the control and treatment groups to determine if the treatment group is better than the control group.')
+st.markdown('---')
+
+
+col_1 = st.columns(2)
+with col_1[0]:
+    control_visitors = st.number_input('Control Visitors', min_value=0, max_value=100000, value=2)
+with col_1[1]:
+    control_conversions = st.number_input('Control Conversions', min_value=0, max_value=100000, value=2)
+
+col_2 = st.columns(2)
+with col_2[0]:
+    treatment_visitors = st.number_input('Treatment Visitors', min_value=0, max_value=100000, value=2)
+with col_2[1]:
+    treatment_conversions = st.number_input('Treatment Conversions', min_value=0, max_value=100000, value=8)
+
+col_3 = st.columns(3)
+with col_3[0]:
+    confidence_level = st.selectbox('Conficence Level (%)', options=[90, 95, 99], index=1)
+
+
+# Perform A/B Test
+if st.button('Perform A/B Test'):
+    result = perform_ab_test(control_visitors, control_conversions, treatment_visitors, treatment_conversions, confidence_level)
+    if result == "Experiment Group is Better":
+        st.success(result)
+    elif result == "Control Group is Better":
+        st.error(result)
+    else:
+        st.warning(result)  
+
---
+++ pages/sample_size.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+
+
+st.set_page_config(layout="wide")
+
+def calculate_sample_size(alpha, beta, baseline_conversion, mde):
+    # Standard normal critical values at alpha/2 and 1 - beta
+    z_alpha_2 = norm.ppf(1 - alpha / 2)
+    z_beta = np.abs(norm.ppf(1 - beta))
+    
+    # Calculate sample size
+    sample_size = ((z_alpha_2 + z_beta)**2 * baseline_conversion * (1 - baseline_conversion) )/ (mde**2)
+    
+    return int(round(sample_size)) *  2
+
+st.title('Fitfly - Data Science Intern Assignment')
+st.subheader('Sample Size Calculator 📏')
+st.markdown('Enter the baseline conversion rate and the minimum detectable effect to calculate the sample size needed for an A/B test.')
+st.markdown('---')
+
+col_1 = st.columns(2)
+with col_1[0]:
+    baseline_conversion = st.number_input('Baseline Conversion Rate', min_value=0.0, max_value=1.0, value=0.2, step=0.01)
+with col_1[1]:
+    mde = st.number_input('Minimum Detectable Effect', min_value=0.0, max_value=1.0, value=0.05, step=0.01)
+
+col_2 = st.columns(2)
+with col_2[0]:
+    alpha = st.selectbox('Significance Level', options=[0.01, 0.05, 0.10], index=1)
+with col_2[1]:
+    beta = st.selectbox('Power', options=[0.9, 0.8, 0.7, 0.6], index=1)
+
+# Calculate sample size
+if st.button('Calculate Sample Size'):
+    sample_size = calculate_sample_size(alpha, beta, baseline_conversion, mde)
+    # st.metric(label="Sample Size :", value=sample_size)
+    st.markdown('Sample Size :')
+    col_3 = st.columns([4,2,4])
+    with col_3[1]:
+        st.markdown(f"# :rainbow[{sample_size}] :balloon:")
+    st.markdown('---')
+    st.markdown('**Note:** The sample size calculated is the total for both groups (control and treatment).')
